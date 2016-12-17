58072ea24cac0dd66d2c728457de8a25a6b2c05e
Moepl <github@mcom.moepl.eu>
2016-12-17 23:01:57 +0100

    Added startpage

--- sunknightsapp/views/views.py
+++ sunknightsapp/views/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from ..models.clan_user import ClanUser
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    users=ClanUser.objects.all()
+    context={'noobs':users}
+    return render(request,'index.html',context)
