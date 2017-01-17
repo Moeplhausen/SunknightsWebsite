6a210f58a28e69d788fb45e08a9b666d6b239a5f
Moepl <github@mcom.moepl.eu>
2017-01-17 16:54:15 +0100

    also give is_superuser info on userfast api

--- sunknightsapp/serializers/clan_user_serializer.py
+++ sunknightsapp/serializers/clan_user_serializer.py
@@ -71,7 +71,7 @@ class ClanUserFasterSerializer(BulkSerializerMixin,serializers.ModelSerializer):
 
     class Meta:
         model=ClanUser
-        fields=('id','avatar','discord_id','discord_nickname','is_active','roles','discord_discriminator','pointsinfo')
+        fields=('id','avatar','discord_id','discord_nickname','is_active','roles','discord_discriminator','pointsinfo','is_superuser')
         list_serializer_class = BulkListSerializer
 
 
