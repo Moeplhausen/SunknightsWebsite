fa9c3ccef6f9b469fd9b8a268b8b6307d844b2ee
Pg Biel <pgbiel@hotmail.com.br>
2017-02-02 18:10:04 -0200

    Switch md to window.md and fix indent

--- sunknightsapp/static/sunknightsapp/js/loadmd.js
+++ sunknightsapp/static/sunknightsapp/js/loadmd.js
@@ -1,59 +1,64 @@
 //Helper for loading markdown in the info files
-var md;
+try {
+  window.md;
+} catch (err) {
+  //no window.md? then create one
+  window.md = undefined;
+}
 
 var sh = new showdown.Converter({
-    simplifiedAutoLink: true,
-    excludeTrailingPunctuationFromURLs: true,
-    strikethrough: true,
-    tasklists: true,
-    requireSpaceBeforeHeadingText: true,
-    tables: true,
-    simpleLineBreaks: true
+  simplifiedAutoLink: true,
+  excludeTrailingPunctuationFromURLs: true,
+  strikethrough: true,
+  tasklists: true,
+  requireSpaceBeforeHeadingText: true,
+  tables: true,
+  simpleLineBreaks: true
 });
 function loadMd(markdown) {
-    md = markdown.replace(/&/g, "&amp;");
+  md = markdown.replace(/&/g, "&amp;");
 }
 function mdToHtml(element,helptext) {
-    if (helptext) {
-        console.log('lo?');
-        $(element).html(sh.makeHtml(helptext));
-    }
-    return !!helptext;
+  if (helptext) {
+    console.log('lo?');
+    $(element).html(sh.makeHtml(helptext));
+  }
+  return !!helptext;
 }
 function editor(elementID, button, sbuttonID) {
-    var idselect = function (theid) {
-        return "#" + theid;
-    };
-    var thing = $(idselect(elementID));
-    var cancelb = $(idselect(sbuttonID));
-    button = $(button);
-    var oldhtml = thing.html();
-    thing.html("<textarea rows='10' cols='30'>" + md + "</textarea>");
-    button.html("Submit");
-    button.attr("onclick", "editsubmit(" + elementID + ", this, " + sbuttonID);
-    cancelb[0].style.display = "block";
+  var idselect = function (theid) {
+    return "#" + theid;
+  };
+  var thing = $(idselect(elementID));
+  var cancelb = $(idselect(sbuttonID));
+  button = $(button);
+  var oldhtml = thing.html();
+  thing.html("<textarea rows='10' cols='30'>" + md + "</textarea>");
+  button.html("Submit");
+  button.attr("onclick", "editsubmit(" + elementID + ", this, " + sbuttonID);
+  cancelb[0].style.display = "block";
 }
 function editsubmit(elementID, button, sbuttonID) {
-    var idselect = function (theid) {
-        return "#" + theid;
-    };
-    var thing = $(idselect(elementID));
-    var oldhtml = thing.html();
-    $.post(window.location.href, {
-        newmd: oldhtml
-    }, function (d, s) {
-        console.log(`Status: ${s} (Debug)`);
-        window.location = window.location.href;
-    });
+  var idselect = function (theid) {
+    return "#" + theid;
+  };
+  var thing = $(idselect(elementID));
+  var oldhtml = thing.html();
+  $.post(window.location.href, {
+    newmd: oldhtml
+  }, function (d, s) {
+    console.log(`Status: ${s} (Debug)`);
+    window.location = window.location.href;
+  });
 }
 function canceledit(elementID, buttonID, sbutton) {
-    window.location = window.location.href;
-    /*var idselect = function(theid){ return "#" + theid; };
-     var thing = $(idselect(elementID));
-     var button = $(idselect(buttonID));
-     var oldhtml = thing.html();
-     thing.html(mdToHtml(idselect(elementID)));
-     button.html("Edit");
-     button.attr("onclick", "editor("+elementID+", this, "+ sbutton.attr("id"));
-     sbutton[0].style.display = "none";*/
+  window.location = window.location.href;
+  /*var idselect = function(theid){ return "#" + theid; };
+  var thing = $(idselect(elementID));
+  var button = $(idselect(buttonID));
+  var oldhtml = thing.html();
+  thing.html(mdToHtml(idselect(elementID)));
+  button.html("Edit");
+  button.attr("onclick", "editor("+elementID+", this, "+ sbutton.attr("id"));
+  sbutton[0].style.display = "none";*/
 }
