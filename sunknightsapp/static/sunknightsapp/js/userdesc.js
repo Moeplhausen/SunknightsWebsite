751eb5e2eb8b53fd0aa936ec0a806539bea29bff
Pg Biel <pgbiel@hotmail.com.br>
2017-02-05 11:39:23 -0200

    Life

--- sunknightsapp/static/sunknightsapp/js/userdesc.js
+++ sunknightsapp/static/sunknightsapp/js/userdesc.js
@@ -14,10 +14,31 @@ try {
   });
 }
 function getDesc(selector, md) {
-  md = md.replace(/&/g, "&amp;");
+  md = md.replace(/&(?![^\s;];)/g, "&amp;");
+  window.descmd = md;
   var descNode = $(selector);
   var newhtml = sh.makeHtml(md);
   descNode.html(newhtml);
 }
 
-function setDesc(selector, inputselec) { /*WIP*/ }
+function editDesc(button, cancelsel, selector/*, inputselec*/) {
+  var descNode = $(selector);
+  var submit = $("#" + button.id);
+  var cancel = $(cancelsel);
+  //var input = $(inputselec);
+  descNode.html("<textarea class='form-control' id='descinput' rows='10' >"+descmd+"</textarea>");
+  submit.html("Submit").attr("onclick", "setDesc(\"#descinput\")");
+  cancel.css("display", "block");
+}
+
+function cancelDesc(cancelbutton, buttonsel, selector) {
+  var descNode = $(selector);
+  var button = $(buttonsel);
+  cancelbutton.css("display", "none");
+  button.html("Edit").attr("editDesc(this, \"#" + cancelbutton.id + "\", \"" + selector + "\")");
+  getDesc(selector, descmd);
+}
+
+function setDesc(selector) {
+  //WIP
+}
