f428f6ddbc6e0da2764d6c4ebdafbd6153cb2908
Moepl <github@mcom.moepl.eu>
2017-02-14 15:39:40 +0100

    give info when bonus quests can be done

--- sunknightsapp/templatetags/task_quests.py
+++ sunknightsapp/templatetags/task_quests.py
@@ -11,11 +11,15 @@ def cut(value, arg):
 def task_checked(user, task:QuestTask):
         q=QuestTask.objects.filter(id=task.id,eventquest__pointsinfo__user_id=user.id)
 
+        if not q and task.tier==4:
+            return '<i class="fa fa-question-circle" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="Bonus quests can be selected when at least 3 other daily quests have been finished"></i>'
+
         if not q:
             return ""
 
         if q.filter(eventquest__accepted=True):
             return '<i class="fa fa-check" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="You already completed that quest :)"></i>'
+
 
         return '<i class="fa fa-question-circle" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="Your submission will be handled by a Points Manager shortly"></i>'
 
