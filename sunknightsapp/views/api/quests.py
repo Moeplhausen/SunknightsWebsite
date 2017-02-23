c0d539218a405045d28d744b14ce97704e63bb1b
Moepl <github@mcom.moepl.eu>
2017-02-24 00:05:30 +0100

    Only send the permanent the current daily quest

--- sunknightsapp/views/api/quests.py
+++ sunknightsapp/views/api/quests.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
 from ...serializers.daily_quest_serializer import QuestSerializer
 from ...models.daily_quest import Quest
+import datetime
 
 
 class QuestsViewSet(viewsets.ModelViewSet):
     serializer_class = QuestSerializer
-    queryset = Quest.objects.all()
+
+    from django.db.models import Q
+    now = (datetime.datetime.utcnow()).replace(hour=0, minute=0, second=0, microsecond=0)
+
+    queryset = Quest.objects.filter(Q(permed=True)|Q(date=now))
