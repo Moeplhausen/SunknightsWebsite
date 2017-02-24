4bc8db6dd8c8b153ca95509495db404a07decba7
Moepl <github@mcom.moepl.eu>
2017-02-24 15:57:08 +0100

    maybe that is the actual fix for the last problem

--- sunknightsapp/views/api/quests.py
+++ sunknightsapp/views/api/quests.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
 from ...serializers.daily_quest_serializer import QuestSerializer
 from ...models.daily_quest import Quest
-import datetime
+
 
 
 class QuestsViewSet(viewsets.ModelViewSet):
+    import datetime
     serializer_class = QuestSerializer
 
     from django.db.models import Q
