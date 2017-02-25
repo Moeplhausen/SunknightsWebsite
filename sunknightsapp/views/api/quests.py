f8975f098b205122b389e1f608ddd95f9ddaa862
Moepl <github@mcom.moepl.eu>
2017-02-25 10:25:18 +0100

    another attempt for utcnow not updating

--- sunknightsapp/views/api/quests.py
+++ sunknightsapp/views/api/quests.py
@@ -1,13 +1,19 @@
 from rest_framework import viewsets
 from ...serializers.daily_quest_serializer import QuestSerializer
 from ...models.daily_quest import Quest
+import datetime
 
-
+def getCorrectTime(date=None):
+    if date is None:
+        return datetime.datetime.utcnow()
 
 class QuestsViewSet(viewsets.ModelViewSet):
-    import datetime
+
+
+
+
     serializer_class = QuestSerializer
 
     from django.db.models import Q
 
-    queryset = Quest.objects.filter(Q(permed=True)|Q(date=(datetime.datetime.utcnow()).replace(hour=0, minute=0, second=0, microsecond=0)))
+    queryset = Quest.objects.filter(Q(permed=True)|Q(date=(getCorrectTime(None)).replace(hour=0, minute=0, second=0, microsecond=0)))
