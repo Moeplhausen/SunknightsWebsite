c611d16cb8a38af1d3e8c142833cabba65a00bee
William Zhou <wzhouwzhou@gmail.com>
2017-03-09 22:34:32 -0500

    Trash commit
    Backup - March 9 - suggestions

--- sunknightsapp/forms/preferences_forms.py
+++ sunknightsapp/forms/preferences_forms.py
@@ -1,8 +1,22 @@
-from .base_form import BaseForm
-from ..enums.AjaxActions import AjaxAction
-from ..models.clan_user import ClanUserPreferences
-from ..serializers.clan_user_serializer import ClanUserPreferencesSerializer
 
+##============================================================================================##
+##{{{begin imports
+
+#--
+from .base_form import BaseForm
+#--
+from ..enums.AjaxActions import AjaxAction
+#--
+from ..models.clan_user import ClanUserPreferences
+#--
+from ..serializers.clan_user_serializer import ClanUserPreferencesSerializer
+#--
+
+##end imports}}}
+##============================================================================================##
+
+##============================================================================================##
+##{{{begin class SubmitPreferencesForm:
 class SavePreferencesForm(BaseForm):
     def __init__(self, *args, **kwargs):
         super(SavePreferencesForm, self).__init__(AjaxAction.SAVEPREFERENCES, *args, **kwargs)
@@ -21,8 +35,13 @@ class SavePreferencesForm(BaseForm):
         print(serializer.data)
         return self.response(True, {'data': (serializer.data)})
 
-
-
+    ##==========================================================================================##
+    ##{{{begin class Meta:
     class Meta:
         model = ClanUserPreferences
         fields = ('custom_background_enabled', 'custom_background_url',)
+    ##end class Meta}}}
+    ##============================================================================================##
+
+##end class SubmitPreferencesForm}}}
+##============================================================================================##
