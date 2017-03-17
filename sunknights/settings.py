6fd626f84aed6e4e985a7f60649f0986f5f7478e
Moepl <github@mcom.moepl.eu>
2017-03-17 18:35:56 +0100

    Made preference tab visible again

--- sunknights/settings.py
+++ sunknights/settings.py
@@ -69,6 +69,8 @@ REST_FRAMEWORK = {
     #'PAGE_SIZE': 10,
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 150,
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAdminUser'
     ],
