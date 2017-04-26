d8f776a5e563637b747e48d6c472b4007b0e6fa1
Moepl <github@mcom.moepl.eu>
2017-04-26 18:03:31 +0200

    use discord widget for invite

--- sunknightsapp/models/discord_server.py
+++ sunknightsapp/models/discord_server.py
@@ -4,7 +4,6 @@ from django.db import models
 class DiscordServer(models.Model):
     discord_id=models.CharField(max_length=20,unique=True)
     name=models.CharField(max_length=30)
-    invite_link=models.CharField(max_length=50,default='')
 
 
     def __str__(self):
