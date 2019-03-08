9a979748cc16492238085cc8a703dc2c37ebaa72
olaamigocr7@gmail.com <hibahyfa123>
2019-03-09 00:37:09 +0300

    ?setquests command changed to work with weekly quests

--- sunknightsapp/views/api/quests.py
+++ sunknightsapp/views/api/quests.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets
 from ...serializers.daily_quest_serializer import QuestSerializer
 from ...models.daily_quest import Quest
 import datetime
+from datetime import timedelta
+
 
 
 class QuestsViewSet(viewsets.ModelViewSet):
@@ -11,6 +13,6 @@ class QuestsViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         from django.db.models import Q
-        return Quest.objects.filter(Q(permed=True)|Q(date=datetime.datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)))
+        return Quest.objects.filter(Q(permed=True)|Q(date=(datetime.datetime.utcnow()- timedelta(days=datetime.datetime.utcnow().weekday())).replace(hour=0, minute=0, second=0, microsecond=0)))
 
 
